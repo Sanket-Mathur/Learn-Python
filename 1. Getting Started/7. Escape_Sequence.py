3c4207bf8bbe3d7a73adc04b2f492fc0f4c28a17
Sanket Mathur <rajeev.sanket@gmail.com>
2020-06-06 15:33:01 +0530

    Added 7. Escape_Sequence.py

---
+++ 1. Getting Started/7. Escape_Sequence.py
@@ -0,0 +1,13 @@
+"""
+Escape sequences are special characters that tells the interpreter that a particular character is not meant to be a part of the code.
+For example if you want to print an single quote ' in a string but you are also wrapping the string in ''.
+There are a few escape sequences in python, We will see some of them in the examples.
+"""
+
+print('I\'m Sanket Mathur and you are reading the code on my GitHub')
+
+print('I want some new lines\nYup, like this\nAnd one more\nAnd another one')
+
+print('A horizontal tab looks\tlike this')
+
+print('A vertical tab looks\vlike this')
